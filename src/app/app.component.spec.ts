5530f3876460c260540d14bf53c12ae76160b124
elsa.de.alfonso <elsa.de.alfonso@gmail.com>
2023-01-09 23:09:31 +0100

    app de ejemplo teoria

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'punk-beer'`, () => {
+  it(`should have as title 'angular-pwa-introduction'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('punk-beer');
+    expect(app.title).toEqual('angular-pwa-introduction');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('punk-beer app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-pwa-introduction app is running!');
   });
 });
